cf1003a7abd28d970612306a1aa99abfa7e13aa4
saik <themindplays@gmail.com>
2021-03-19 16:43:45 +0200

    Dried script and, main css layout

--- script.js
+++ script.js
@@ -1,54 +1,33 @@
-function computerPlay (){
-    const choices = ['rock', 'paper', 'scissors'];
-    return choices [Math.floor(Math.random() * choices.length)];
-    }
+function computerPlay() {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
 
-    let playerScore = 0;
-    let computerScore = 0;
- 
-function playRound (playerSelection, computerSelection){
-   
-       if (playerSelection === 'rock'){            // rock selection
-            if (computerSelection === 'rock'){
-                    return 'Draw'
-            } else if (computerSelection ==='paper'){
-                    return 'You lose!' + ++computerScore
-            } else (computerSelection ==='scissors')
-                    return 'You win!' + ++playerScore
+let playerScore = 0;
+let computerScore = 0;
 
-     } else if (playerSelection ==='paper'){        // paper selection
-             if (computerSelection ==='paper'){
-                     return 'Draw'
-             } else if (computerSelection ==='scissors'){
-                     return 'You lose!' + ++computerScore
-             } else (computerSelection ==='rock')
-                     return 'You win!' + ++playerScore
-            
-     } else if (playerSelection ==='scissors'){      // scissor selection
-             if (computerSelection ==='scissors'){
-                     return 'Draw' 
-             } else if (computerSelection ==='rock'){
-                     return 'You lose!' + ++computerScore
-             } else (computerSelection ==='paper')
-                     return 'You win!' + ++playerScore
-     } return 'Please return a valid answer'
-   };
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) return "draw";
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `You win! Player: ${++playerScore} ; Computer: ${computerScore}`;
+  } else
+    return `Computer win! Player: ${playerScore} ; Computer: ${++computerScore}`;
+}
 
-function game(){
-        for (i = 1; ; i++){
-                                               
-                  let ps = prompt('Rock, paper or scissors?')
-                  ps = ps.toLowerCase()
-                  const cs = computerPlay()
-                  console.log(playRound(ps, cs)) 
-                                              
-                  if (playerScore === 5) 
-                           break
-                    else if (computerScore === 5) 
-                           break       
-                    else   continue
-                        
-                } 
-        }
+function game() {
+  for (i = 1; ; i++) {
+    // let playerSelection = prompt("Rock, paper or scissors?");
+    playerSelection = playerSelection.toLowerCase();
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
 
-        console.log(game())
+    if (playerScore === 5 || computerScore === 5) break;
+    else continue;
+  }
+}
+
+console.log(game());
